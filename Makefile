74c4f3a652e71930909239dc6bcda4ca54a3a513
Tom Barlow <tomwbarlow@gmail.com>
2017-04-15 23:03:47 +0100

    Use saucelabs to run selenium tests
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- Makefile
+++ Makefile
@@ -7,42 +7,53 @@ REPO?=merritt/$(APP)
 TAG?=latest
 DEPS=$(shell go list ./... | grep -v /vendor/)
 
+BUILD_OUTPUT_DIR?=build
+
 UI_BUILD_IMAGE?=merritt-ui-build:$(COMMIT)
-UI_SRCS=$(shell find ui -type f \
+UI_BUILD_SRCS=$(shell find ui -type f \
+				-not -path "ui/e2e/*" \
 				-not -path "ui/node_modules/*")
+UI_BUILD_OUTPUT_DIR?=$(BUILD_OUTPUT_DIR)/ui
 
 all: build build/ui
 
 build:
+	@echo "Building binary: $(BUILD_OUTPUT_DIR)/$(APP)"
 	@go build \
 		-ldflags "-w -X github.com/$(REPO)/version.GitCommit=$(COMMIT)" \
-		-o build/$(APP) \
+		-o $(BUILD_OUTPUT_DIR)/$(APP) \
 		./cmd/$(APP)
 
 build-static:
+	@echo "Building static binary: $(BUILD_OUTPUT_DIR)/$(APP)"
 	@go build -a \
 		-tags "netgo static_build" \
 		-installsuffix netgo \
 		-ldflags "-w -X github.com/$(REPO)/version.GitCommit=$(COMMIT)" \
-		-o build/$(APP) \
+		-o $(BUILD_OUTPUT_DIR)/$(APP) \
 		./cmd/$(APP)
 
-build-ui-image: $(UI_SRCS)
+build-ui-image: $(UI_BUILD_SRCS)
+	@echo "Building UI build image: $(UI_BUILD_IMAGE)"
 	@docker build \
 		-t $(UI_BUILD_IMAGE) \
 		-f ui/Dockerfile.ui \
 		ui
 
 build/ui: build-ui-image
+	@echo "Extracting UI build image to $(UI_BUILD_OUTPUT_DIR): $(UI_BUILD_IMAGE)"
 	@mkdir -p build/ui && \
-		docker run --rm -i $(UI_BUILD_IMAGE) | tar xvzf - -C build/ui
+		docker run --rm -i $(UI_BUILD_IMAGE) | tar xvzf - -C $(UI_BUILD_OUTPUT_DIR)
 
 image: build-static build/ui
+	@echo "Building $(REPO):$(TAG) image"
 	@docker build \
 		-t $(REPO):$(TAG) \
-		.
+		. && \
+		docker tag $(REPO):$(TAG) $(REPO):$(COMMIT)
 
 release: image
+	@echo "Releasing $(REPO):$(TAG)"
 	@docker push $(REPO):$(TAG)
 
 check:
@@ -55,4 +66,4 @@ test: build
 clean:
 	@rm -rf build
 
-.PHONY: all build-ui-image build-static release check test clean
+.PHONY: all build build-static build-ui-image image release check test clean
---
+++ hack/ui-test.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+set -e
+
+if [ -z "$SAUCE_USERNAME" ]; then
+    echo "Need to set SAUCE_USERNAME"
+    exit 1
+fi  
+
+if [ -z "$SAUCE_ACCESS_KEY" ]; then
+    echo "Need to set SAUCE_ACCESS_KEY"
+    exit 1
+fi
+
+COMMIT=`git rev-parse --short HEAD`
+RUN_ID=$(cat /dev/random | LC_CTYPE=C tr -dc "[:lower:]" | head -c 8)
+
+MERRITT_IMAGE_NAME=merritt/merritt:${COMMIT}
+UI_E2E_IMAGE_NAME=merritt-ui-e2e
+SAUCE_CONNECT_IMAGE_NAME=sauce-connect
+
+MERRITT_CONTAINER_NAME=merritt-${RUN_ID}
+SAUCE_CONTAINER_NAME=sauce-${RUN_ID}
+TEST_CONTAINER_NAME=ui-test-${RUN_ID}
+NETWORK_NAME=ui-test-${RUN_ID}
+
+SAUCE_URL=http://${SAUCE_CONTAINER_NAME}:4445/
+TEST_URL=http://${MERRITT_CONTAINER_NAME}:8080/
+
+function url_ready {
+    echo "Waiting for $1 to be ready..."
+
+    URL_STATUS=$(
+        docker run --rm -it \
+            --network ${NETWORK_NAME} \
+            mbentley/curl --write-out %{http_code} --silent --output /dev/null $1
+    ) || true
+
+    if [ "$URL_STATUS" -eq 200 ]
+    then
+        # 0 = true
+        return 0 
+    else
+        # 1 = false
+        return 1
+    fi
+}
+
+# Cleanup on exit
+function finish {
+    echo "Cleaning up containers/networks"
+    docker rm -fv ${MERRITT_CONTAINER_NAME} || true
+    docker rm -fv ${SAUCE_CONTAINER_NAME} || true
+    docker network rm ${NETWORK_NAME}
+}
+trap finish EXIT
+
+docker network create ${NETWORK_NAME} || true
+
+docker run -it -d \
+    --name ${MERRITT_CONTAINER_NAME} \
+    --network ${NETWORK_NAME} \
+    ${MERRITT_IMAGE_NAME} \
+    -D run
+
+RETRY_TIMEOUT=24
+RETRY_ATTEMPTS=0
+until url_ready ${TEST_URL} || [ $RETRY_ATTEMPTS -eq $RETRY_TIMEOUT ]; do
+   RETRY_ATTEMPTS=$((RETRY_ATTEMPTS++))
+   sleep 5s
+done
+
+echo "Building ${UI_E2E_IMAGE_NAME} image for ui tests"
+docker build \
+    -t ${UI_E2E_IMAGE_NAME} \
+    -f ui/e2e/Dockerfile.e2e \
+    ui/e2e
+
+echo "Building ${SAUCE_CONNECT_IMAGE_NAME} image for saucelabs connection"
+docker build \
+    -t ${SAUCE_CONNECT_IMAGE_NAME} \
+    -f ui/e2e/Dockerfile.sauceconnect \
+    ui/e2e
+
+echo "Running ${SAUCE_CONNECT_IMAGE_NAME}"
+docker run -d -it \
+    --name ${SAUCE_CONTAINER_NAME} \
+    --network ${NETWORK_NAME} \
+	-e SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} \
+	-e SAUCE_USERNAME=${SAUCE_USERNAME} \
+	${SAUCE_CONNECT_IMAGE_NAME} -v
+
+# Wait for sauce connect to become ready
+RETRY_TIMEOUT=24
+RETRY_ATTEMPTS=0
+until url_ready ${SAUCE_URL} || [ $RETRY_ATTEMPTS -eq $RETRY_TIMEOUT ]; do
+   RETRY_ATTEMPTS=$((RETRY_ATTEMPTS++))
+   sleep 5s
+done
+
+echo "Running ${UI_E2E_IMAGE_NAME}"
+docker run --rm -it \
+    --name ${TEST_CONTAINER_NAME} \
+    --network ${NETWORK_NAME} \
+	-e SAUCE_ACCESS_KEY=${SAUCE_ACCESS_KEY} \
+	-e SAUCE_USERNAME=${SAUCE_USERNAME} \
+    -e TEST_URL=${TEST_URL} \
+	${UI_E2E_IMAGE_NAME} $*
