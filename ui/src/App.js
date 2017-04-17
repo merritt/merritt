3981957cb089f99390d680e432c969c4b4fb5bbb
Tom Barlow <tomwbarlow@gmail.com>
2017-04-17 07:33:38 +0100

    Run UI unit tests (jest) on Travis
    Signed-off-by: Tom Barlow <tomwbarlow@gmail.com>

--- ui/src/App.js
+++ ui/src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Merritt</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
